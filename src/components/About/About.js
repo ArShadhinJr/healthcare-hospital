11c516523e95387699164cf063ea5a3856ebbf42
Abdur Rahman BIn Muhammad <arshadhinjr@gmail.com>
2021-10-20 23:34:09 +0600

    commit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,24 +10,24 @@ function About() {
                 <p>HEALTCARE HOSPITAL / AIMS, OBJECTIVES AND VISION</p>
             </div>
             {/* About us details */}
-            <div className="px-5 m-5 ">
-                <p className="text-justify pb-5">The success of hospitals and healthcare providers depends on the quality of their service. But in an age where most of us look up information online before making almost any decision, a well-designed healthcare website is no less important.
+            <div className="px-5 px-sm-2 m-5 m-sm-2 ">
+                <p className="text-justify pb-5 pb-sm-2">The success of hospitals and healthcare providers depends on the quality of their service. But in an age where most of us look up information online before making almost any decision, a well-designed healthcare website is no less important.
 
                     Most of the people visit a hospital website to get information. So these sites must be very user-friendly, easy to navigate and extremely well-organised with a pleasing aesthetics.
 
                     Based on these criteria we’ve listed some of the best medical and healthcare websites, mostly from Australia. Any website for a doctor’s clinic or a hospital can follow the good parts of these sites.</p>
-                <p className="text-justify pb-5">The website of the Royal Melbourne Hospital combines traditional website design with the usability best practices. The result is a regal and trustworthy look that goes well with their storied history of 170 years.
+                <p className="text-justify pb-5 pb-sm-2">The website of the Royal Melbourne Hospital combines traditional website design with the usability best practices. The result is a regal and trustworthy look that goes well with their storied history of 170 years.
 
                     They present all the information that a patient or general visitor might need in great details. This makes their homepage very actionable and encourages the visitors to interact with the site.</p>
-                <p className="text-justify pb-5">The first thing that pops up to your mind when viewing this site is how uplifting the design is. High-quality positive imagery is used throughout the site to send a positive message. The website provides information related to their services and timetable. There is also in-depth information about various physical problems.
+                <p className="text-justify pb-5 pb-sm-2">The first thing that pops up to your mind when viewing this site is how uplifting the design is. High-quality positive imagery is used throughout the site to send a positive message. The website provides information related to their services and timetable. There is also in-depth information about various physical problems.
 
                     A combination of soothing colour scheme and friendly images makes it one of the best medical websites in Australia.</p>
-                <p className="text-justify pb-5">The modern design of The Alfred focuses on highlighting the positive impact of their physicians and researchers. Their homepage shows many powerful images of medical professionals helping their patients.
+                <p className="text-justify pb-5 pb-sm-2">The modern design of The Alfred focuses on highlighting the positive impact of their physicians and researchers. Their homepage shows many powerful images of medical professionals helping their patients.
 
                     Positive and active images are used throughout the site while presenting all the necessary information. Thanks to a balanced use of text contents and visuals, it never feels cluttered.
 
                     They maintain a specific colour theme and design style in all their pages giving the site a unified brand voice.</p>
-                <p className="text-justify pb-5">This hospital website utilises the ‘feel-good’ aspect of human perception. Like their logo of a lotus, the homepage gives off a sense of serenity thanks to its beautiful use of white space.
+                <p className="text-justify pb-5 pb-sm-2">This hospital website utilises the ‘feel-good’ aspect of human perception. Like their logo of a lotus, the homepage gives off a sense of serenity thanks to its beautiful use of white space.
 
                     The same quality is maintained throughout the site. Visitors are able to collect their necessary information easily and quickly.</p>
                 <p>Thank You</p>
